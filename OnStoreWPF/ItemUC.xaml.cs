9564a08ed2e4d4426ec6fd41bfdcb7c31b9f3e85
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-15 01:52:55 +0400

    Add project files.

---
+++ OnStoreWPF/ItemUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OnStoreWPF
+{
+    /// <summary>
+    /// Interaction logic for ItemUC.xaml
+    /// </summary>
+    public partial class ItemUC : UserControl
+    {
+        public ItemUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OnStoreWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace OnStoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PreviewKeyDown += HandleEsc;
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Escape) return;
+            Close();
+        }
+
+        private void Close_Click(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void ControlBox_Click(object sender, MouseButtonEventArgs e)
+        {
+            var elp = sender as Ellipse;
+
+            switch (elp.Name)
+            {
+                case "close":
+                    Close();
+                    break;
+                case "max":
+                    WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+                    break;
+                default:
+                    WindowState = WindowState.Minimized;
+                    break;
+            }
+        }
+    }
+}
